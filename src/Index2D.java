287b41a4b2314e9b9e6d829fbf504ae88774d2b9
Ofek <ofekson12@gmail.com>
2026-01-11 20:12:19 +0200

    Testing 20 speed

--- src/Index2D.java
+++ src/Index2D.java
@@ -1,8 +1,16 @@
-public class Index2D implements Pixel2D {
+public class Index2D implements Pixel2D{
     private int _x, _y;
-    public Index2D() {this(0,0);}
-    public Index2D(int x, int y) {_x=x;_y=y;}
-    public Index2D(Pixel2D t) {this(t.getX(), t.getY());}
+    public Index2D() {
+        this(0,0);
+    }
+    public Index2D(int x, int y) {
+        _x=x;_y=y;
+    }
+
+    public Index2D(Pixel2D t) {
+        this(t.getX(), t.getY());
+    }
+
     @Override
     public int getX() {
         return _x;
@@ -11,13 +19,18 @@ public class Index2D implements Pixel2D {
     public int getY() {
         return _y;
     }
+    @Override
     public double distance2D(Pixel2D t) {
-        if (t == null) {
-            throw new IllegalArgumentException("p2 Can't be null!");}
-        int dx = (this._x - t.getX());
-        int dy = (this._y - t.getY());
-        double distance = Math.sqrt(dx*dx - dy*dy);
-        return distance;
+        double ans = 0;
+        if(t==null) {//check if the parameter 't' is null
+            throw new RuntimeException("invalid paramter");
+        }
+        double dx = _x- t.getX();// Calculate the difference in X coordinates between the current pixel and 't'
+        double dy =_y-t.getY(); // Calculate the difference in Y coordinates between the current pixel and 't'
+        double dis=((dx*dx)+(dy*dy)); //Compute the squared distance using the formula (dx * dx) + (dy * dy)
+
+        ans=Math.sqrt(dis);
+        return ans;
     }
     @Override
     public String toString() {
@@ -26,12 +39,16 @@ public class Index2D implements Pixel2D {
     @Override
     public boolean equals(Object t) {
         boolean ans = false;
-       /////// you do NOT need to add your code below ///////
-        if(t instanceof Pixel2D) {
-            Pixel2D p = (Pixel2D) t;
-            ans = (this.distance2D(p)==0);
+        // Check if the object is null or not an instance of Index2D
+        if(t==null || !(t instanceof Index2D)) {
+
+            return false;
         }
-       ///////////////////////////////////
+        // Cast the object to Index2D type for further comparison
+        Index2D t2 = (Index2D)t;
+        ans = (_x == t2._x) && (_y == t2._y);
+
         return ans;
     }
+
 }
